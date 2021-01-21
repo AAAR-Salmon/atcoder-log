632cda9bdce4914d960e6d049d3890549b0d2575
AAAR Salmon <TenzikuJohn@gmail.com>
2021-01-21 21:55:51 +0900

    ARC111_AをAC

--- arc/arc111/a/main.py
+++ arc/arc111/a/main.py
@@ -1,11 +1,2 @@
-import sys
-import math
-from math import sin, cos, tan
-from functools import reduce
-from collections import deque
-import heapq
-sys.setrecursionlimit(1000000)
-intm1 = lambda x:int(x)-1
-
 N,M = map(int,input().split())
-print(math.floor(10 ** ((N - math.log10(M)) % math.log10(M))) % M)
+print(pow(10, N, M**2) // M % M)

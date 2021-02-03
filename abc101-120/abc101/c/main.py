7fab519f8be36457b3f9ea5dec5eb287a19b3ca0
AAAR Salmon <TenzikuJohn@gmail.com>
2021-02-03 23:38:41 +0900

    ABC101_CをAC

--- abc101-120/abc101/c/main.py
+++ abc101-120/abc101/c/main.py
@@ -1,8 +1,12 @@
-import sys
-import math
-from math import sin, cos, tan
-from functools import reduce
-from collections import deque
-import heapq
-sys.setrecursionlimit(1000000)
-intm1 = lambda x:int(x)-1
+N,K = map(int,input().split())
+A = list(map(int,input().split()))
+i = A.index(1)
+j = i + 1
+ans = 0
+if i % (K - 1) != 0:
+	i = i // (K - 1) * (K - 1)
+	j = i + K
+	ans += 1
+ans += i // (K - 1)
+ans += (N - j + K - 2) // (K - 1)
+print(ans)

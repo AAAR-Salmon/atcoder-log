2402407dd37a82595864b898c826888d19108a3a
AAAR Salmon <TenzikuJohn@gmail.com>
2021-02-15 23:22:58 +0900

    ABC102_CをAC

--- abc101-120/abc102/c/main.py
+++ abc101-120/abc102/c/main.py
@@ -1,8 +1,10 @@
-import sys
-import math
-from math import sin, cos, tan
-from functools import reduce
-from collections import deque
-import heapq
-sys.setrecursionlimit(1000000)
-intm1 = lambda x:int(x)-1
+N = int(input())
+A = list(map(int,input().split()))
+B = [A[i] - i - 1 for i in range(N)]
+B.sort()
+med = B[N // 2]
+if N % 2 != 1:
+	med = (med + B[N // 2 - 1]) // 2
+	
+print(min(sum(abs(B[i] - med) for i in range(N)),
+          sum(abs(B[i] - med - 1) for i in range(N))))

e888dd9707864ab7b226c92c9aad192156a52ae4
AAAR Salmon <tenzikujohn@gmail.com>
2021-03-10 00:02:15 +0900

    ABC173_ABをAC

---
+++ abc161-180/abc173/b/main.py
@@ -0,0 +1,17 @@
+N = int(input())
+AC = WA = TLE = RE = 0
+for _ in range(N):
+	s = input()
+
+	if s == "AC":
+		AC += 1
+	elif s == "WA":
+		WA += 1
+	elif s == "TLE":
+		TLE += 1
+	elif s == "RE":
+		RE += 1
+print(f"AC x {AC}")
+print(f"WA x {WA}")
+print(f"TLE x {TLE}")
+print(f"RE x {RE}")

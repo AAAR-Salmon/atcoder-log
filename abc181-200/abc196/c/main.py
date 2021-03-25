7a1eb01838bdfbf263d694c06a6d01915a5d6741
AAAR Salmon <TenzikuJohn@gmail.com>
2021-03-25 22:39:24 +0900

    ABC196_Cをリファクタリング

--- abc181-200/abc196/c/main.py
+++ abc181-200/abc196/c/main.py
@@ -5,7 +5,16 @@ N_len = len(N)
 ans = 0
 
 if N_len % 2 == 0:
-	ans = max(max(min(int(N[:N_len // 2]), int(N[N_len // 2:])),int(N[:N_len // 2]) - 1) - 10 ** (N_len // 2 - 1) + 1, 0)
+	ans = max(
+		max(
+			min(
+				int(N[:N_len // 2]),
+				int(N[N_len // 2:])
+			),
+			int(N[:N_len // 2]) - 1
+		) - 10 ** (N_len // 2 - 1) + 1,
+		0
+	)
 
 for i in range(1, (N_len + 1) // 2):
 	ans += (10 ** i - 1) - 10 ** (i - 1) + 1

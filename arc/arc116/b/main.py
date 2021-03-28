2486ce5cc3a816bb584f85a807aa797c9dcf286f
AAAR Salmon <TenzikuJohn@gmail.com>
2021-03-29 04:50:06 +0900

    ARC116_BをAC

--- arc/arc116/b/main.py
+++ arc/arc116/b/main.py
@@ -5,10 +5,9 @@ A = list(map(int,input().split()))
 A.sort()
 
 ans = 0
-for i in range(N):
-	ans = (ans + A[i] * A[i]) % MOD
-	pow2 = 1
-	for j in range(i + 1, N):
-		ans = (ans + A[i] * A[j] * pow2) % MOD
-		pow2 = (pow2 * 2) % MOD
+
+S = 0
+for i in range(N - 1, -1, -1):
+	ans = (ans + A[i] * (A[i] + S)) % MOD
+	S = (2 * S + A[i]) % MOD
 print(ans)

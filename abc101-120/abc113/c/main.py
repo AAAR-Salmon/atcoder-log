2c7992780785437f0be1c9eff17d67f123e711a0
AAAR-Salmon <tenzikujohn@gmail.com>
2021-05-10 22:43:02 +0900

    ABC113_CをAC

--- abc101-120/abc113/c/main.py
+++ abc101-120/abc113/c/main.py
@@ -1,3 +1,17 @@
-import math
-from functools import reduce
-from collections import deque
+from heapq import heapify, heappush, heappop
+
+N,M = map(int,input().split())
+listPref = [[] for _ in range(N+1)]
+for i in range(M):
+	P,Y = map(int,input().split())
+	heappush(listPref[P], (Y, i))
+ids = [None] * M
+for pref in range(1, N+1):
+	x = 1
+	prefStr = str(pref).zfill(6)
+	while listPref[pref]:
+		city = heappop(listPref[pref])[1]
+		ids[city] = prefStr + str(x).zfill(6)
+		x += 1
+for id in ids:
+	print(id, flush=False)

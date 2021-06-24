7a0c203feaf5c4fa00054f3dda9a11ed021e9159
AAAR-Salmon <tenzikujohn@gmail.com>
2021-06-24 22:57:39 +0900

    ABC107 CをAC

---
+++ abc101-120/abc107/c/main.py
@@ -0,0 +1,7 @@
+N, K = map(int, input().split())
+x = list(map(int, input().split()))
+
+ans = 1 << 60
+for i in range(N - K + 1):
+	ans = min(ans, min(abs(x[i]), abs(x[i + K - 1])) + abs(x[i + K - 1] - x[i]))
+print(ans)
